730d022902a54753b7bdfdcb8557e0355537fb2e
bossiernesto <bossi.ernestog@gmail.com>
2014-11-17 14:45:37 -0300

    kaleidoscope parser, lexer complete, end of Chapter 2 Tutorial

--- lexer.ml
+++ lexer.ml
@@ -35,7 +35,7 @@ and lex_number buffer = parser
       lex_number buffer stream
   | [< stream=lex >] ->
       [< 'Token.Number (float_of_string (Buffer.contents buffer)); stream >]
-			
+
 and lex_ident buffer = parser
   | [< ' ('A' .. 'Z' | 'a' .. 'z' | '0' .. '9' as c); stream >] ->
       Buffer.add_char buffer c;
@@ -49,5 +49,4 @@ and lex_ident buffer = parser
 and lex_comment = parser
   | [< ' ('\n'); stream=lex >] -> stream
   | [< 'c; e=lex_comment >] -> e
-  | [< >] -> [< >]			
-			
+  | [< >] -> [< >]
